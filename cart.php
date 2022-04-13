6773890412b0f717475b19f2e8c66d2aee9c1218
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-13 12:58:56 +0200

    Added all file from other project

---
+++ cart.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DeveloperLand - Het meest leerzame prepark voor het hele gezin!</title>
+
+  <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+  <header>
+    <div class="header-content">
+      <div class="wrapper">
+        <img src="images/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
+        <h1 class="hidden-on-lg">DeveloperLand</h1>
+      </div>
+    </div>
+  </header>
+  <main>
+    <div class="wrapper">
+      <article>
+        <h2>Winkelwagentje</h2>
+        <div>
+          <span>Dit zijn alle producten die u gaat bestellen. Controleer een laatste keer of alles klopt en ga daarna verder met de grote groene knop.</span>
+          <table>
+            <tr class="tableHeader">
+              <th>Product</th>
+              <th>Prijs (enkel)</th>
+              <th>Aantal</th>
+              <th>Prijs (totaal)</th>
+            </tr>
+            <tr>
+              <td>Eentreekaartje Kind t/m 3 jaar</td>
+              <td>&euro; 0</td>
+              <td>1</td>
+              <td>&euro; 0</td>
+            </tr>
+            <tr>
+              <td>Eentreekaartje Regulier</td>
+              <td>&euro; 24,95</td>
+              <td>2</td>
+              <td>&euro; 49,90</td>
+            </tr>
+            <tr class="tableHeader">
+              <td>Eentreekaartje Senior</td>
+              <td>&euro; 14,95</td>
+              <td>3</td>
+              <td>&euro; 14,95</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td><strong>Totaalsom<br>(incl. btw)</strong></td>
+              <td>&euro; 64,85</td>
+            </tr>
+          </table>
+          
+          <button><a href="payment.html">Dit is correct, verder naar betaling</a></button>
+        </div>
+      </article>
+    </div>
+  </main>
+  <footer>
+    <div class="wrapper">
+      <div class="copyright">
+        &copy; 2020 - 2021  DeveloperLand&nbsp;&nbsp;All Rights Reserved
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ product.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DeveloperLand - Het meest leerzame prepark voor het hele gezin!</title>
+
+  <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+  <header>
+    <div class="header-content">
+      <div class="wrapper">
+        <img src="images/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
+        <h1 class="hidden-on-lg">DeveloperLand</h1>
+      </div>
+    </div>
+  </header>
+  <main>
+    <div class="wrapper">
+      <article>
+        <h3>DeveloperLand mok</h3>
+      </article>
+        <div class="details">
+          <div class="img">
+            <img src="images/merchandise/mok.JPG" alt="Een Mok">
+          </div>
+          <div class="msg">
+            <p><strong>Lorem ipsum dolor sit</strong> amet consectetur, adipisicing elit. Aperiam, rem.<br>
+            <strong>ipsum dolor sit</strong> amet consectetur, adipisicing elit. Perferendis itaque <strong>possimus ipsam distinctio</strong> amet suscipit deleniti officia, soluta voluptates, reprehenderit natus iusto, adipisci fugiat mollitia.</p>
+            <p>
+              <button><a href="cart.html">Bestel nu de tickets (&euro; 24,95)</a></button>
+            </p>
+          </div>
+        </div>
+    </div>
+  </main>
+  <footer>
+    <div class="wrapper">
+      <div class="copyright">
+        &copy; 2020 - 2021  DeveloperLand&nbsp;&nbsp;All Rights Reserved
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
