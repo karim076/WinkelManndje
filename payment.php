71acb7846e38a22add1260e8394734d7cdd53ab6
Sharmake-Omar <93530529+Sharmake-Omar@users.noreply.github.com>
2022-04-20 13:16:59 +0200

    img toegepast

--- payment.php
+++ payment.php
@@ -11,7 +11,7 @@
   <header>
     <div class="header-content">
       <div class="wrapper">
-        <img src="images/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
+        <img src="img/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
         <h1 class="hidden-on-lg">DeveloperLand</h1>
       </div>
     </div>
@@ -25,22 +25,22 @@
         <div class="sidy-by-side">
           <div>
             <a href="javascript:alert('Dit valt buiten de scope van dit project');">
-              <img src="images/ideal-logo-1024.png" alt="Met iDeal betalen" class="payment-option">
+              <img src="img/ideal-logo-1024.png" alt="Met iDeal betalen" class="payment-option">
             </a>
           </div>
           <div>
             <a href="javascript:alert('Dit valt buiten de scope van dit project');">
-              <img src="images/visa-logo-1024.png" alt="Met Visa betalen" class="payment-option">
+              <img src="img/visa-logo-1024.png" alt="Met Visa betalen" class="payment-option">
             </a>
           </div>
           <div>
             <a href="javascript:alert('Dit valt buiten de scope van dit project');">
-              <img src="images/mastercard-logo-1024.png" alt="Met Mastercard betalen" class="payment-option">
+              <img src="img/mastercard-logo-1024.png" alt="Met Mastercard betalen" class="payment-option">
             </a>
           </div>
           <div>
             <a href="javascript:alert('Dit valt buiten de scope van dit project');">
-              <img src="images/paypal-logo-1024.png" alt="Met PayPal betalen" class="payment-option">
+              <img src="img/paypal-logo-1024.png" alt="Met PayPal betalen" class="payment-option">
             </a>
           </div>
         </div>
