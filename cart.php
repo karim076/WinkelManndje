2c76c8a123443c3ab3cd850c18a5b85dbfc8b611
Sharmake-Omar <93530529+Sharmake-Omar@users.noreply.github.com>
2022-04-20 13:16:30 +0200

    table toegevoegd

--- cart.php
+++ cart.php
@@ -11,7 +11,7 @@
   <header>
     <div class="header-content">
       <div class="wrapper">
-        <img src="images/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
+        <img src="imag/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
         <h1 class="hidden-on-lg">DeveloperLand</h1>
       </div>
     </div>
@@ -22,39 +22,62 @@
         <h2>Winkelwagentje</h2>
         <div>
           <span>Dit zijn alle producten die u gaat bestellen. Controleer een laatste keer of alles klopt en ga daarna verder met de grote groene knop.</span>
-          <table>
-            <tr class="tableHeader">
-              <th>Product</th>
-              <th>Prijs (enkel)</th>
-              <th>Aantal</th>
-              <th>Prijs (totaal)</th>
-            </tr>
+          <?php
+
+require_once 'backend/config.php';  
+
+$winkelwagentje = [
+    6 => 2,
+    7 => 1,
+];
+
+$hoeveelInWinkelwagentje = count($winkelwagentje);
+
+$totalPrice = 0;
+
+?>
+<table>
+    <thead>
+        <tr>
+            <th>Product</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Amount</th>
+            <th>Total Price</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    <?php
+
+        foreach($winkelwagentje as $productId => $amount){
+            $stmt = $db->prepare("SELECT * FROM products WHERE id = ". $productId .";");
+            $stmt->execute();
+            $product = $stmt->fetch(PDO::FETCH_ASSOC);
+            $totalProductPrice = $product['price'] * $amount;
+            $totalPrice = $totalPrice + $totalProductPrice;
+
+            ?>
             <tr>
-              <td>Eentreekaartje Kind t/m 3 jaar</td>
-              <td>&euro; 0</td>
-              <td>1</td>
-              <td>&euro; 0</td>
+                <td><?php echo $product['name']; ?></td>
+                <td><?php echo $product['description']; ?></td>
+                <td>&euro;<?php echo $product['price']; ?>,-</td>
+                <td><?php echo $amount ?></td>
+                <td>&euro;<?php echo $totalProductPrice; ?>,-</td>
             </tr>
-            <tr>
-              <td>Eentreekaartje Regulier</td>
-              <td>&euro; 24,95</td>
-              <td>2</td>
-              <td>&euro; 49,90</td>
-            </tr>
-            <tr class="tableHeader">
-              <td>Eentreekaartje Senior</td>
-              <td>&euro; 14,95</td>
-              <td>3</td>
-              <td>&euro; 14,95</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td><strong>Totaalsom<br>(incl. btw)</strong></td>
-              <td>&euro; 64,85</td>
-            </tr>
-          </table>
-          <button><a href="payment.html">Dit is correct, verder naar betaling</a></button>
+            <?php
+        }
+    ?>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>&euro;<?php echo $totalPrice; ?>,-</td>
+    </tr>
+    </tbody>
+</table>
+          <button><a href="payment.php">Dit is correct, verder naar betaling</a></button>
         </div>
       </article>
     </div>
