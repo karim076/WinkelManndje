9e03f8768c9c00bd40078e7fe68fb2df4f47bf69
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 16:34:39 +0200

    Added links to head

---
+++ head.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="styles/main.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<title>DeveloperLand - Het meest leerzame prepark voor het hele gezin!</title>
