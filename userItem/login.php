9747b2b503f386b40c972f1ff9567c8c39657917
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 17:41:49 +0200

    Added require and footer

--- userItem/login.php
+++ userItem/login.php
@@ -11,14 +11,14 @@
   <div class="container">
 	<div class="screen">
 		<div class="screen__content">
-			<form class="login">
+			<form class="login" method="POST" action="../backend/loginController.php">
 				<div class="login__field">
 					<i class="login__icon fas fa-user"></i>
-					<input type="text" class="login__input" name="username" placeholder="Username">
+					<input type="text" class="login__input" name="username" placeholder="Username" required>
 				</div>
 				<div class="login__field">
 					<i class="login__icon fas fa-lock"></i>
-					<input type="password" class="login__input" name="pass" placeholder="Wachtwoord">
+					<input type="password" class="login__input" name="pass" placeholder="Wachtwoord" required>
 				</div>
 				<input type="submit" content="Log In Now" class="button login__submit">
 				</button>				
@@ -31,6 +31,13 @@
 			<span class="screen__background__shape screen__background__shape1"></span>
 		</div>		
 	</div>
-</div>  
+</div>
+<footer>
+    <div class="wrapper">
+      <div class="copyright">
+        &copy; 2020 - 2021 DeveloperLand&nbsp;&nbsp;All Rights Reserved
+      </div>
+    </div>
+  </footer>  
 </body>
 </html>
