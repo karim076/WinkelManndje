c41a3c41066a7f58382e3b1455e525ddc219a38b
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 19:18:51 +0200

    Added new field email

--- userItem/register.php
+++ userItem/register.php
@@ -18,14 +18,22 @@
 					<i class="login__icon fas fa-user"></i>
 					<input type="text" class="login__input" name="username" placeholder="Username" required>
 				</div>
+
         <div class="login__field">
 					<i class="login__icon fas fa-user"></i>
 					<input type="text" class="login__input" name="name" placeholder="naam" required>
 				</div>
+
+        <div class="login__field">
+					<i class="login__icon fas fa-envelope"></i>
+					<input type="email" class="login__input" name="email" placeholder="Email" required>
+				</div>
+        
 				<div class="login__field">
 					<i class="login__icon fas fa-lock"></i>
 					<input type="password" class="login__input" name="pass" placeholder="Wachtwoord" required>
 				</div>
+
 				<input type="submit"  class="button login__submit">
 				</button>				
 			</form>
