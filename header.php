a6b23dcd4504d9bfde8839099b081f607bfa7bae
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 16:59:58 +0200

    Fixed path to login, register and image link

--- header.php
+++ header.php
@@ -2,13 +2,13 @@
 <div class="navbar">
     <a class="active" href="<?php echo $base_url?>/index.php"><i class="fa fa-fw fa-home"></i> Home</a>
     <div class="left">
-        <a href="<?php echo $base_url?>/register.php"><i class="fa-solid fa-user-plus"></i> register</a>
-        <a href="<?php echo $base_url?>/login.php"><i class="fa fa-fw fa-user"></i> Login</a>
+        <a href="<?php echo $base_url?>/userItem/register.php"><i class="fa-solid fa-user-plus"></i> register</a>
+        <a href="<?php echo $base_url?>/userItem/login.php"><i class="fa fa-fw fa-user"></i> Login</a>
     </div>
 </div>
 <div class="header-content">
     <div class="wrapper">
-        <img src="img/logo.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
+        <img src="<?php echo $base_url?>/img/logo.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
         <h1 class="hidden-on-lg">DeveloperLand</h1>
     </div>
 </div>
