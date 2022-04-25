985cc19c756ce3fe3c17ea5990997ffbe4cf2915
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 20:48:39 +0200

    Added adminPaneel page

---
+++ adminPanel/adminPaneel.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require_once '../head.php' ?>
+</head>
+<body>
+    <header>
+        <?php require_once '../header.php' ?>
+    </header>
+</body>
+</html>
