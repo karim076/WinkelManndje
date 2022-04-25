1c9de1047f2a34ded5016340184918944352e996
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 16:59:22 +0200

    Added right path

--- userItem/register.php
+++ userItem/register.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <?php require_once 'head.php' ?>
+  <?php require_once '../head.php' ?>
   <title>DeveloperLand - Het meest leerzame prepark voor het hele gezin!</title>
 </head>
 <body>
   <header>
-    <?php require_once 'header.php' ?>
+    <?php require_once '../header.php' ?>
   </header>   
 </body>
 </html>
