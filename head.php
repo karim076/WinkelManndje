a7e77e9fa40ec6144d40c11bcf9c0f70baf2d9bb
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 17:00:31 +0200

    Added global user styling and right path to backend

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
+<?php require_once 'backend/config.php' ?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="styles/main.css">
+<link rel="stylesheet" href="<?php echo $base_url?>/styles/main.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
