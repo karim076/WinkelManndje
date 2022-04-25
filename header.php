4cff8aba080006e72a3e4e81415b7d91e982324d
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 18:24:57 +0200

    Fixed header doesnt show when logged in

--- header.php
+++ header.php
@@ -5,13 +5,13 @@
     <a class="active" href="<?php echo $base_url?>/index.php"><i class="fa fa-fw fa-home"></i> Home</a>
     <div class="left">
 <?php   error_reporting(E_ERROR | E_WARNING | E_PARSE);
-        session_start(); 
+        session_start();
         if(isset($_SESSION['user_id'])): ?>
+            <i class="fa fa-fw fa-user"></i>Welcome,<?php echo $_SESSION['name'] ?>
+            <a href="<?php echo $base_url?>/userItem/logout.php">Uitloggen</a>
+        <?php else: ?>
             <a href="<?php echo $base_url?>/userItem/register.php"><i class="fa-solid fa-user-plus"></i> register</a>
             <a href="<?php echo $base_url?>/userItem/login.php"><i class="fa fa-fw fa-user"></i> Login</a>
-        <?php else: ?>
-            <i class="fa fa-fw fa-user"></i>Welcome,<?php echo $_SESSION['username'] ?>
-            <a href="<?php echo $base_url?>/userItem/logout.php">Log uit</a>
         <?php endif; ?>
     </div>
 </div>
