1405b08b1f9c630438ee8466823bef57357f141a
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-25 16:44:29 +0200

    Added basic html and included header and head

---
+++ register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require_once 'head.php' ?>
+  <title>DeveloperLand - Het meest leerzame prepark voor het hele gezin!</title>
+</head>
+<body>
+  <header>
+    <?php require_once 'header.php' ?>
+  </header>   
+</body>
+</html>
