b3c51b9719a63ad563b975ee41d45a911b2b1381
Sharmake-Omar <93530529+Sharmake-Omar@users.noreply.github.com>
2022-04-26 14:43:44 +0200

    beschrijving producten er bij genoteerd

--- backend/conn.php
+++ backend/conn.php
@@ -4,5 +4,5 @@
 require_once 'config.php';
 
 //Met behulp van PDO zetten we de connectie op, waarna we met setAttribute de manier van errormeldingen weergeven bepalen.
-$conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
