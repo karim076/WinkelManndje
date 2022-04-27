25224751126d54eefb328459753d022130b524d7
Sharmake-Omar <93530529+Sharmake-Omar@users.noreply.github.com>
2022-04-27 17:07:45 +0200

    added tabel

--- adminPanel/adminPaneel.php
+++ adminPanel/adminPaneel.php
@@ -7,11 +7,116 @@
     <header>
         <?php require_once '../header.php' ?>
     </header>
-    <main>
-        
-    </main>
-    <footer>
+    <?php
+        require_once '../backend/conn.php';
+        $query = "SELECT * FROM taken WHERE status = 'todo'";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $todo = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+    /*<!-- Ophalen -->*/
+        $query = "SELECT * FROM taken WHERE status = 'progress'";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $progress = $statement->fetchAll(PDO::FETCH_ASSOC);
+    /*<!-- Verzonden -->*/
+
+        $query = "SELECT * FROM taken WHERE status = 'done'";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $done = $statement->fetchAll(PDO::FETCH_ASSOC);
+        /*var_dump($todo); testing*/
+    ?>
+    <main>
+		<!-- <a href="create.php">Nieuw Task</a> -->
+		<div class="flex1">
+			<a href="create.php" class="btn">Nieuw Task</a>
+			<a href="personalTasks.php" class="btn">My tasks</a>
+		</div>
+		<div class="wrapper">
+			<div class="todo">
+				<h1>To-Do</h1>
+				<div class="table scroll tableFixHead">
+					<table>
+						<thead>
+				            <tr class="solid">
+				                <th>status</th>
+								<th>email</th>
+								<th>order_email</th>
+				                <th>order_number</th>
+				            </tr>
+			            </thead>
+			            <?php foreach($todo as $taken): ?>
+			            <tbody>			                <tr>
+			                    <?php $id = $taken['id'] ?>
+			                    <td><?php echo $taken['status']; ?></td>
+								<td><?php echo $taken['product']; ?></td>
+								<td><?php echo $taken['email_recipient']; ?></td>
+			                    <td><?php echo $taken['order_number']; ?></td>
+			                    <td><a href="edit.php?id=<?php echo $id;?>">Aanpassen</a></td>
+			                </tr>
+			            </tbody>
+	            		<?php endforeach; ?>
+	        		</table>
+				</div>
+			</div>
+			<div class="progress">
+				<h1>Progress</h1>
+				<div class="table scroll tableFixHead">
+					<table>
+						<thead>
+				            <tr>
+								<th>Titel</th>
+								<th>Naam</th>
+								<th>Asignee</th>
+				                <th>Bericht</th>
+				            </tr>
+				        </thead>
+			            <?php foreach($progress as $doing): ?>
+			            	<tbody>
+				                <tr>
+				                    <?php $id = $doing['id'] ?>
+				                    <td><?php echo $doing['status']; ?></td>
+									<td><?php echo $doing['product']; ?></td>
+									<td><?php echo $doing['email_recipient']; ?></td>
+				                    <td><?php echo $doing['order_number']; ?></td>
+				                    <td><a href="edit.php?id=<?php echo $id;?>">Aanpassen</a></td>
+				                </tr>
+				            </tbody>
+	            		<?php endforeach; ?>
+	        		</table>
+				</div>
+			</div>
+			<div class="done">
+				<h1>Done</h1>
+				<div class="table scroll tableFixHead">
+					<table>
+						<thead>
+				            <tr>
+								<th>Titel</th>
+								<th>Naam</th>
+								<th>Asignee</th>
+				                <th>Bericht</th>
+				            </tr>
+			            </thead>
+			            <?php foreach($done as $klaar): ?>
+			            	<tbody>
+				                <tr>
+				                    <?php $id = $klaar['id'] ?>
+				                    <td><?php echo $klaar['statys']; ?></td>
+									<td><?php echo $klaar['naam']; ?></td>
+									<td><?php echo $klaar['asignee']; ?></td>
+				                    <td><?php echo $klaar['beschrijving']; ?></td>
+				                    <td><a href="edit.php?id=<?php echo $id;?>">Aanpassen</a></td>
+				                </tr>
+				            </tbody>
+	            		<?php endforeach; ?>
+	        		</table>
+				</div>
+			</div>
+		</div>
+	</main>
+    <footer>
     </footer>
 </body>
 </html>
