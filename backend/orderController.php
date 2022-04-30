f439c4e96f76bc968d875ad289ac7a8816fac54e
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-30 21:42:37 +0200

    Added insert with join testing

--- backend/orderController.php
+++ backend/orderController.php
@@ -1,10 +1,31 @@
 <?php 
 session_start();
-if(isset($_SESSION['user_id']))
+if(!isset($_SESSION['user_id']))
 {
     die("Geen toegang!");    
 }
 $email_recipient = $_SESSION['email'];
-$order_number = rand(100,100000) + "R" + rand(100,100000);
-
+$order_number = rand(100,1000) + rand(0,100);
+$id_Product = $_POST['id'];
+//1. Verbinding
+require_once 'conn.php';
+$query = "INSERT INTO orders(email_recipient,order_number) 
+              VALUES(:email_recipient,:order_number)
+            --   SELECT product
+            --   FROM orders
+              RIGHT JOIN products 
+              AS product 
+              ON orders.product = products.product WHERE products.id = $id_Product";
+    
+//3. Prepare
+$statement=$conn->prepare($query);
+//4. Execute
+$statement->execute
+    ([
+        ":email_recipient" => $email_recipient,
+        ":order_number" => $order_number
+    ]);
+$msg = "Product is in behandeling";
+header("location: $base_url/index.php?msg=$msg");
+exit;
 ?>
