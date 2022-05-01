2f54763668dda1768c44ed5ffecb3083205e95f5
karim076 <59794989+karim076@users.noreply.github.com>
2022-05-01 19:59:53 +0200

    Deleted right join Added product from session

--- backend/orderController.php
+++ backend/orderController.php
@@ -5,27 +5,25 @@ if(!isset($_SESSION['user_id']))
     die("Geen toegang!");    
 }
 $email_recipient = $_SESSION['email'];
-$order_number = rand(100,1000) + rand(0,100);
+$order_number = rand(100,1000) + rand(10,10000);
 $id_Product = $_POST['id'];
+$product = $_SESSION['product'];
 //1. Verbinding
 require_once 'conn.php';
-$query = "INSERT INTO orders(email_recipient,order_number) 
-              VALUES(:email_recipient,:order_number)
-              SELECT product
-              FROM products
-              WHERE :id = $id_Product
-              RIGHT JOIN orders 
-              ON orders.product = products.product";
+$query = "INSERT INTO orders(product,email_recipient,order_number) 
+          VALUES(:product,:email_recipient,:order_number)";
     
 //3. Prepare
 $statement=$conn->prepare($query);
 //4. Execute
 $statement->execute
     ([
+        ":product" => $product,
         ":email_recipient" => $email_recipient,
         ":order_number" => $order_number
     ]);
 $msg = "Product is in behandeling";
+unset($_SESSION['product']);
 header("location: $base_url/index.php?msg=$msg");
 exit;
 ?>
