7030b70192acd33ac3f41f0a8dcbac9d953ce15d
karim076 <59794989+karim076@users.noreply.github.com>
2022-05-04 14:26:19 +0200

    Added Update set " Done "

--- backend/adminController.php
+++ backend/adminController.php
@@ -1,6 +1,19 @@
 <?php 
-
-hier je moet de update en set invoeren
+$_action = $_POST['action'];
+$id = $_POST['id'];
+$status = $_POST['status'];
+if($_action == "edit")
+{
+    require_once 'conn.php';
+    $query = "UPDATE orders SET status = :status 
+              WHERE :id = $id";
+    $statement = $conn->prepare($query);
+    $statement->execute([
+        ":status" => $status,
+        ":id" => $id
+    ]);
+        header("Location: $base_url/adminPanel/adminPaneel.php?msg=succevolveranderd");
+    }
 
 
 ?>
