5d7d3446508ec16afa770113927e00592c4956b5
karim076 <59794989+karim076@users.noreply.github.com>
2022-05-10 16:51:50 +0200

    .

--- backend/orderController.php
+++ backend/orderController.php
@@ -4,13 +4,12 @@ if(!isset($_SESSION['user_id']))
 {
     die("Geen toegang!");    
 }
-$_action = $_POST['action'];
 $email_recipient = $_SESSION['email'];
 $order_number = rand(100,1000) + rand(10,10000);
 $id_Product = $_POST['id'];
 $product = $_SESSION['product'];
-if($_action == "order gemaakt")
-{
+
+
     //1. Verbinding
 require_once 'conn.php';
 $query = "INSERT INTO orders(product,email_recipient,order_number) 
@@ -28,20 +27,6 @@ $statement->execute
 $msg = "Product is in behandeling";
 unset($_SESSION['product']);
 header("location: $base_url/index.php?msg=$msg");
-}
-
-
-    if($_action == "edit")
-    {
-        require_once 'conn.php';
-        $query = "UPDATE orders SET status = :status, WHERE id= :id";
-        $statement = $conn->prepare($query);
-        $statement->execute([
-            ":status" => $status,
-            ":id" => $id,
-        ]);
-        header("Location: ./adminPanel/adminPaneel.php?msg=taak");
-    }
 
 exit;
 ?>
