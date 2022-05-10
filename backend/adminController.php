01e411676f618cc8efad233056e8407b7c2bb88f
karim076 <59794989+karim076@users.noreply.github.com>
2022-05-10 16:51:28 +0200

    Added security

--- backend/adminController.php
+++ backend/adminController.php
@@ -1,19 +1,24 @@
 <?php 
-$_action = $_POST['action'];
+session_start();
+if(!isset($_SESSION['user_id']))
+{
+    die("Geen toegang!");    
+}
+
 $id = $_POST['id'];
 $status = $_POST['status'];
-if($_action == "edit")
-{
-    require_once 'conn.php';
-    $query = "UPDATE orders SET status = :status 
-              WHERE :id = $id";
-    $statement = $conn->prepare($query);
-    $statement->execute([
-        ":status" => $status,
-        ":id" => $id
-    ]);
-        header("Location: $base_url/adminPanel/adminPaneel.php?msg=succevolveranderd");
-    }
+
+require_once 'conn.php';
+$query = "UPDATE (id, status) FROM orders
+          SET status = :status 
+          WHERE :id = $id"
+          ;
+$statement = $conn->prepare($query);
+$statement->execute([
+    ":status" => $status,
+    ":id" => $id
+]);
+header("Location: $base_url/adminPanel/adminPaneel.php?msg=succevolveranderd");
 
 
 ?>
